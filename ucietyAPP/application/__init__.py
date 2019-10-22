f3cf9168fe43d5de101e718ce137d9a5c4c4fc85
solomonbada <55092939+solomonbada@users.noreply.github.com>
2019-10-22 17:47:06 +0100

    Bootstrapped + Fixed Filter For Account Page

--- ucietyAPP/application/__init__.py
+++ ucietyAPP/application/__init__.py
@@ -1,18 +1,22 @@
+#Importation of relevant modules needed
+
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-
-
-
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-
+Bootstrap(app)
+#the secret key is needed to keep the client-side session secure
 app.config['SECRET_KEY'] = '6732vbu3d3237bdjbkwqebd239739323'
+####
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+'''The login manager contains the code that lets your application
+and Flask-login work together'''
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
--- ucietyAPP/application/forms.py
+++ ucietyAPP/application/forms.py
@@ -1,3 +1,5 @@
+#Importation of relevant modules needed
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField, SelectField, Form, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -8,7 +10,7 @@ from flask_login import LoginManager, current_user, UserMixin
 ######################STUDENT REGISTRATION FORM#############################
 
 class StudentRegistrationForm(FlaskForm):
-	first_name = StringField('First Name',
+	first_name = StringField('First Name', #Plain text fields to enter user credentials
 		validators=[
 			DataRequired()
 		])
@@ -22,7 +24,7 @@ class StudentRegistrationForm(FlaskForm):
 	names = []
 
 	for i in range(int(len(lists))):
-		temp = [lists[i].id, lists[i].uni_name] #id, uni_name
+		temp = [lists[i].id, lists[i].uni_name] #Produces a drop down list which passes a integer through to the database to enable relationship between tables
 		names.append(temp)
 
 	uni_id = SelectField("University",
@@ -34,7 +36,7 @@ class StudentRegistrationForm(FlaskForm):
 	names = []
 
 	for i in range(int(len(lists))):
-		temp = [lists[i].uni_name, lists[i].uni_name] #uni_name
+		temp = [lists[i].uni_name, lists[i].uni_name] #Produces a drop down list which passes a string through to the database
 		names.append(temp)
 
 	uni_name = SelectField("Confirm University",
@@ -52,16 +54,16 @@ class StudentRegistrationForm(FlaskForm):
 	confirm_password = PasswordField('Confirm Password',
 		validators=[
 			DataRequired(),
-			EqualTo('password')
+			EqualTo('password') #Ensures that the password that has been re-entered is equivalent to the inital entry
 		])
 	submit = SubmitField('Sign Up')
 
-	def validate_email(self, email):
+	def validate_email(self, email): #A function which ensures duplicate emails are not present in the database
 		user=StudentUsers.query.filter_by(email=email.data).first()
 		if user:
 			raise ValidationError('Email already in use!')
 
-
+##########################################################################
 ######################STUDENT LOGIN FORM####################################
 
 class StudentLoginForm(FlaskForm):
@@ -74,9 +76,11 @@ class StudentLoginForm(FlaskForm):
 		validators=[
 			DataRequired()
 		])
-	remember = BooleanField('Remember Me')
+	remember = BooleanField('Remember Me') #checkbox that allows the app to remember the specific user
 	submit = SubmitField('Login')
 
+##########################################################################
+######################UPDATE ACCOUNT FORM####################################
 
 class UpdateAccountForm(FlaskForm):
 	first_name = StringField('First Name',
@@ -94,33 +98,32 @@ class UpdateAccountForm(FlaskForm):
 			Email()
 		])
 
-	lists = Society.query.filter_by(uni_id=StudentUsers.uni_id).all()
-	#[(1, 'A'), (2, 'B'), (3, 'C')]
+	lists = Society.query.filter_by(uni_id=StudentUsers.uni_id).all() #Queries the database to present results in a dropdown format of all univeristy societies for the current user
 	names = []
 
 	for i in range(int(len(lists))):
-		temp = [lists[i].SocietyName, lists[i].SocietyName] #SocietyName
-		names.append(temp)
+		temp = [lists[i].SocietyName, lists[i].SocietyName] 
+		names.append(temp) #This method adds a the previous item (name) to the existing list of society names
 
 	SocietyName = SelectField("Confirm University",
-		choices=names) 
+		choices=names)
 
 	submit = SubmitField('Update')
 
-	def validate_email(self, email):
+	def validate_email(self, email): #A function which ensures duplicate emails are not present in the database
 		if email.data != current_user.email:
 			user=StudentUsers.query.filter_by(email=email.data).first()
 			if user:
 				raise ValidationError('Email already in use! - Please choose another')
 
-######################NOTES FORM#############################
+######################NOTES FORM###################################################
 
 class NotesForm(FlaskForm):
 	title = StringField('Title', 
 		validators=[
 			DataRequired()
 		])
-	content = TextAreaField('Content',
+	content = TextAreaField('Content', #Text area field gives the user better UI Visualisation in regards to what they're inputting
 		validators=[
 			DataRequired()
 		])
