67963837e13e1bc5151cfd3b39e98a3d95ac1bea
solomonbada <55092939+solomonbada@users.noreply.github.com>
2019-10-24 01:18:27 +0100

    Delete SOCDATA.py

--- ucietyAPP/application/SOCDATA.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import sqlite3
-
-con = sqlite3.connect('site.db')
-
-df = pd.read_csv('~/Documents/ucietyAPP/application/SocietyDatabase.csv')
-df.to_sql('~/Documents/ucietyAPP/application/SocietyDatabase.csv', con)
-
-print (df)
